850e23d46003f9349e6a849466007a32607be366
tandat <trantandat0723@gmail.com>
2025-07-14 20:00:47 +0700

    update search

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Navbar() {
     return (
         <div>
             <nav className="flex flex-wrap justify-center gap-2 sm:gap-4 bg-white p-4 rounded shadow">
-                <a href="/"
+                <Link href="/"
                     className="px-4 py-2 rounded-md text-gray-800 hover:text-amber-300  transition font-medium border-b-2 border-transparent hover:border-amber-300">
                     Vấn đáp
-                </a>
-                <a href="/tip"
+                </Link>
+                <Link href="/tip"
                     className="px-4 py-2 rounded-md text-gray-800 hover:text-amber-300  transition font-medium border-b-2 border-transparent hover:border-amber-300">
                     Tip
-                </a>
-                <a href="/profile"
+                </Link>
+                <Link href="/profile"
                     className="px-4 py-2 rounded-md text-gray-800 hover:text-amber-300  transition font-medium border-b-2 border-transparent hover:border-amber-300">
                     Về chúng tôi
-                </a>
+                </Link>
             </nav>
-
         </div>
     );
 }
